b4f06455c27c833a3eb46e9b677c28c127b02df5
John Yi <john.yi@rackspace.com>
2014-10-11 11:16:57 -0400

    re-label package memcache_perf

--- memcache_perf/memcache_perf.go
+++ memcache_perf/memcache_perf.go
@@ -1,42 +1,40 @@
-package main
+package memcache_perf
 
 import (
-        "errors"
-        "flag"
-        "fmt"
-        "log"
-        "strings"
-        "time"
-        "github.com/bradfitz/gomemcache/memcache"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/bradfitz/gomemcache/memcache"
+	"log"
+	"strings"
+	"time"
 )
 
-
-func GetFromMemcache( mc *memcache.Client, key string ) error {
-     defer un(trace("Starting GetFromMemcache"))
-     it,err := mc.Get(key)
-     if err != nil {
-         fmt.Printf("Error: %s", err)
-     } else {
-         fmt.Printf("Retrieving Key %s -> %s\n", it.Key, it.Value )
-     }
-     return nil
+func GetFromMemcache(mc *memcache.Client, key string) error {
+	defer un(trace("Starting GetFromMemcache"))
+	it, err := mc.Get(key)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	} else {
+		fmt.Printf("Retrieving Key %s -> %s\n", it.Key, it.Value)
+	}
+	return nil
 }
 
-func SetToMemcache( mc *memcache.Client, key string, value []byte ) error {
-     mc.Set(&memcache.Item{Key: key, Value: value})
-     return nil
+func SetToMemcache(mc *memcache.Client, key string, value []byte) error {
+	mc.Set(&memcache.Item{Key: key, Value: value})
+	return nil
 }
 
 func trace(s string) (string, time.Time) {
-    log.Println("START:", s)
-    return s, time.Now()
+	log.Println("START:", s)
+	return s, time.Now()
 }
 
 func un(s string, startTime time.Time) {
-    endTime := time.Now()
-    log.Println("  END:", s, "ElapsedTime in seconds:", endTime.Sub(startTime) )
+	endTime := time.Now()
+	log.Println("  END:", s, "ElapsedTime in seconds:", endTime.Sub(startTime))
 }
-
 
 type ServerList []string
 
@@ -45,13 +43,13 @@ func (s *ServerList) String() string {
 }
 
 func (s *ServerList) Set(value string) error {
-    if len(*s) > 0 {
-        return errors.New("memcache flag already set")
-    }
-    for _, server := range strings.Split(value, ",") {
-        *s = append(*s, server)
-    }
-    return nil
+	if len(*s) > 0 {
+		return errors.New("memcache flag already set")
+	}
+	for _, server := range strings.Split(value, ",") {
+		*s = append(*s, server)
+	}
+	return nil
 }
 
 var serverList ServerList
@@ -59,18 +57,18 @@ var key string
 var value string
 
 func init() {
-     flag.Var(&serverList, "memcache", "comma-seperated list of servers")
-     flag.StringVar(&key, "key", "TestKey", "A string for the key")
-     flag.StringVar(&value, "value", "The Cowboys are 4-1 and loving it", "A string for the value")
+	flag.Var(&serverList, "memcache", "comma-seperated list of servers")
+	flag.StringVar(&key, "key", "TestKey", "A string for the key")
+	flag.StringVar(&value, "value", "The Cowboys are 4-1 and loving it", "A string for the value")
 }
 
 func main() {
-    flag.Parse()
-    if serverList == nil {
-        serverList = append(serverList, "localhost:11211")
-    }
-    fmt.Printf("Server list: %s", serverList)
-    mc := memcache.New(strings.Join(serverList,","))
-    SetToMemcache( mc, "TestKey", []byte("Cowboys are 4-1 and we are loving it!") )
-    GetFromMemcache( mc, "TestKey" )
+	flag.Parse()
+	if serverList == nil {
+		serverList = append(serverList, "localhost:11211")
+	}
+	fmt.Printf("Server list: %s", serverList)
+	mc := memcache.New(strings.Join(serverList, ","))
+	SetToMemcache(mc, "TestKey", []byte("Cowboys are 4-1 and we are loving it!"))
+	GetFromMemcache(mc, "TestKey")
 }
