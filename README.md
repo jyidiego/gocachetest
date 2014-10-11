c42ed4f92710d2479bacfe0b3b45093cb98ee1da
John Yi <john.yi@rackspace.com>
2014-10-11 11:13:16 -0400

    Redo memcache

---
+++ README.md
@@ -0,0 +1,4 @@
+cloud_memcache_perf
+===================
+
+A simple command line tool to get a sense of memcache latency
---
+++ memcache_perf/memcache_perf.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+        "errors"
+        "flag"
+        "fmt"
+        "log"
+        "strings"
+        "time"
+        "github.com/bradfitz/gomemcache/memcache"
+)
+
+
+func GetFromMemcache( mc *memcache.Client, key string ) error {
+     defer un(trace("Starting GetFromMemcache"))
+     it,err := mc.Get(key)
+     if err != nil {
+         fmt.Printf("Error: %s", err)
+     } else {
+         fmt.Printf("Retrieving Key %s -> %s\n", it.Key, it.Value )
+     }
+     return nil
+}
+
+func SetToMemcache( mc *memcache.Client, key string, value []byte ) error {
+     mc.Set(&memcache.Item{Key: key, Value: value})
+     return nil
+}
+
+func trace(s string) (string, time.Time) {
+    log.Println("START:", s)
+    return s, time.Now()
+}
+
+func un(s string, startTime time.Time) {
+    endTime := time.Now()
+    log.Println("  END:", s, "ElapsedTime in seconds:", endTime.Sub(startTime) )
+}
+
+
+type ServerList []string
+
+func (s *ServerList) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *ServerList) Set(value string) error {
+    if len(*s) > 0 {
+        return errors.New("memcache flag already set")
+    }
+    for _, server := range strings.Split(value, ",") {
+        *s = append(*s, server)
+    }
+    return nil
+}
+
+var serverList ServerList
+var key string
+var value string
+
+func init() {
+     flag.Var(&serverList, "memcache", "comma-seperated list of servers")
+     flag.StringVar(&key, "key", "TestKey", "A string for the key")
+     flag.StringVar(&value, "value", "The Cowboys are 4-1 and loving it", "A string for the value")
+}
+
+func main() {
+    flag.Parse()
+    if serverList == nil {
+        serverList = append(serverList, "localhost:11211")
+    }
+    fmt.Printf("Server list: %s", serverList)
+    mc := memcache.New(strings.Join(serverList,","))
+    SetToMemcache( mc, "TestKey", []byte("Cowboys are 4-1 and we are loving it!") )
+    GetFromMemcache( mc, "TestKey" )
+}
